7f57802984f1a9d2ab8f171c54265c66e0e52cb8
mtkachenko <oblomov86@gmail.com>
2019-01-27 20:38:49 +0300

    finished unit3

--- src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
+++ src/Unit-3/DoThis/Actors/GithubCommanderActor.cs
@@ -8,7 +8,7 @@ namespace GithubActors.Actors
     /// <summary>
     /// Top-level actor responsible for coordinating and launching repo-processing jobs
     /// </summary>
-    public class GithubCommanderActor : ReceiveActor
+    public class GithubCommanderActor : ReceiveActor, IWithUnboundedStash
     {
         #region Message classes
 
@@ -44,37 +44,85 @@ namespace GithubActors.Actors
 
         #endregion
 
+        private RepoKey _repoJob;
         private IActorRef _coordinator;
         private IActorRef _canAcceptJobSender;
+        private int _pendingJobReplies;
 
         public GithubCommanderActor()
         {
+            Ready();
+        }
+
+        private void Ready()
+        {
             Receive<CanAcceptJob>(job =>
             {
-                _canAcceptJobSender = Sender;
                 _coordinator.Tell(job);
+                _repoJob = job.Repo;
+                BecomeAsking();
             });
+        }
+
+        private void BecomeAsking()
+        {
+            _canAcceptJobSender = Sender;
+            _pendingJobReplies = _coordinator.Ask<Routees>(new GetRoutees()).Result.Members.Count();
+            Become(Asking);
+            Context.SetReceiveTimeout(TimeSpan.FromSeconds(3));
+        }
+
+        private void Asking()
+        {
+            Receive<ReceiveTimeout>(timeout =>
+            {
+                _canAcceptJobSender.Tell(new UnableToAcceptJob(_repoJob));
+                BecomeReady();
+            });
+
+            // stash any subsequent requests
+            Receive<CanAcceptJob>(job => Stash.Stash());
 
             Receive<UnableToAcceptJob>(job =>
             {
-                _canAcceptJobSender.Tell(job);
+                _pendingJobReplies--;
+                if (_pendingJobReplies == 0)
+                {
+                    _canAcceptJobSender.Tell(job);
+                    BecomeReady();
+                }
             });
 
             Receive<AbleToAcceptJob>(job =>
             {
                 _canAcceptJobSender.Tell(job);
 
-                //start processing messages
-                _coordinator.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
+                // start processing messages
+                Sender.Tell(new GithubCoordinatorActor.BeginJob(job.Repo));
 
-                //launch the new window to view results of the processing
-                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+                // launch the new window to view results of the processing
+                Context.ActorSelection(ActorPaths.MainFormActor.Path).Tell(
+                    new MainFormActor.LaunchRepoResultsWindow(job.Repo, Sender));
+
+                BecomeReady();
             });
         }
 
+        private void BecomeReady()
+        {
+            Become(Ready);
+            Stash.UnstashAll();
+            Context.SetReceiveTimeout(null);
+        }
+
+        public IStash Stash { get; set; }
+
         protected override void PreStart()
         {
-            _coordinator = Context.ActorOf(Props.Create(() => new GithubCoordinatorActor()), ActorPaths.GithubCoordinatorActor.Name);
+            _coordinator =
+                Context.ActorOf(Props.Create(() => new GithubCoordinatorActor())
+                    .WithRouter(FromConfig.Instance),
+                    ActorPaths.GithubCoordinatorActor.Name);
             base.PreStart();
         }
 
